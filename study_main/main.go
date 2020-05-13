50d6e42132f14660d90a189a48de48b9b27f0429
CocoVSJugg <lcmgame@163.com>
2020-05-14 00:54:58 +0800

    add and modify

--- study_main/main.go
+++ study_main/main.go
@@ -1,35 +1,10 @@
 package main
 
 import (
-	"fmt"
+//"fmt"
+//"reflect"
 )
 
-type Interface interface {
-	GetStart()
-}
-
-type Implements struct {
-}
-
-func (impl *Implements) GetStart() {
-	fmt.Println("======")
-}
-
-func Test(intf Interface) {
-
-	// _, ok := intf.(Implements)
-	// if ok {
-	// 	fmt.Println("++++++")
-	// } else {
-	// 	fmt.Println("------")
-	// }
-}
-
 func main() {
-
-	var intf Interface
-	//intf = new(Implements)
-	intf = &Implements{}
-	Test(intf)
-
+	//fmt.Println(reflect)
 }
